d5d09071dfb9d9a7151c92ac590f6d7d790e33b8
hbehboudi <hbehboudi@ce.sharif.edu>
2022-09-03 12:16:34 +0430

    Add some symbols

--- commons/src/main/java/edu/sharif/ce/commons/config/Config.java
+++ commons/src/main/java/edu/sharif/ce/commons/config/Config.java
@@ -7,6 +7,7 @@ public class Config {
     public static final String TOPIC = "cryptocurrency";
     public static final String VALUE_CLASS = "valueClass";
     public static final String KEY_CLASS = "keyClass";
-    public static final List<String> SYMBOLS = List.of("ETHBTC", "LTCBTC", "BNBBTC", "NEOBTC", "QTUMETH");
+    public static final List<String> SYMBOLS = List.of("BTCUSDT", "ETHUSDT", "BNBUSDT",
+            "LTCUSDT", "ETHBTC", "BNBBTC", "LTCBTC", "BNBETH", "LTCETH", "LTCBNB");
     public static final List<String> TIME_FRAMES = List.of("1m", "1h", "1d", "1w", "1M");
 }
